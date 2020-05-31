4958297b0cd56c9fc2bfd0f0536604f36f2caa06
Biny <binybing313@gmail.com>
2020-05-31 14:07:15 +0900

    list view

---
+++ studentslist/android/app/src/main/kotlin/com/example/studentslist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.studentslist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ welcome/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+//       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+           title: const Text('My app'),
+         ),
+        body: Center(
+          child: MyWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+  children: <Widget>[
+    Expanded(
+              child: Text(''),
+            ),
+Expanded(
+              child: Text('Welcome',style:TextStyle(fontSize:24)),
+            ),
+ 
+RaisedButton(
+                        child: Text('RaisedButton', style: TextStyle(fontSize: 24)),
+                   
+                    ),
+
+
+  ],
+);
+  }
+}
