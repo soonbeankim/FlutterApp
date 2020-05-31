17957e9bbc5f6a24830f4835d6a200796bc65f8e
Biny <binybing313@gmail.com>
2020-05-31 15:11:31 +0900

    formated!

--- studentslist/lib/main.dart
+++ studentslist/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-final List<int> students = <int>[for(var i=1; i<=100; i+=1) i];
+final List<int> students = <int>[for (var i = 1; i <= 100; i += 1) i];
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,17 +12,15 @@ class MyApp extends StatelessWidget {
       title: title,
       home: Scaffold(
         appBar: AppBar(title: Text(title)),
-        
         body: ListView.builder(
-  padding: const EdgeInsets.all(8),
-  itemCount: students.length,
-  itemBuilder: (BuildContext context, int index) {
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      child: Center(child: Text('Student ${students[index]}')),
-    );
-  }
-),
+            padding: const EdgeInsets.all(8),
+            itemCount: students.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                margin: const EdgeInsets.all(15.0),
+                child: Center(child: Text('Student ${students[index]}')),
+              );
+            }),
       ),
     );
   }
