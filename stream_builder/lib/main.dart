351123d3d35ff5ba15485930483db08d31a07ddb
Biny <binybing313@gmail.com>
2020-06-14 16:55:40 +0900

    latte image & color

--- stream_builder/lib/main.dart
+++ stream_builder/lib/main.dart
@@ -8,26 +8,23 @@
 
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(MyApp());
-
-
-
-
 
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: new ThemeData(accentColor: Colors.green),
-      home: Scaffold(appBar: AppBar(
-      title: const Text('Stream Builder'),
-    ),
-    body: Center(child:MyStatefulWidget(),),
-
-    ));
+        theme: new ThemeData(accentColor: Colors.green),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Stream Builder'),
+            backgroundColor: Colors.green,
+          ),
+          body: Center(
+            child: MyStatefulWidget(),
+          ),
+        ));
   }
 }
 
@@ -39,29 +36,27 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  Future<String> _calculation = Future.delayed(Duration(seconds: 4), () => 'Large Latte');
-
+  Future<String> _calculation =
+      Future.delayed(Duration(seconds: 2), () => 'Large Latte');
 
   Widget build(BuildContext context) {
     return DefaultTextStyle(
       style: Theme.of(context).textTheme.headline2,
       textAlign: TextAlign.center,
-      
-
-      
-      child: FutureBuilder<String>(//////////////////////
+      child: FutureBuilder<String>(
+        //////////////////////
         future: _calculation, // a previously-obtained Future<String> or null
-        
+
         builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
           List<Widget> children;
           if (snapshot.hasData) {
             children = <Widget>[
-               Image(
-            image: NetworkImage('https://globalassets.starbucks.com/assets/b635f407bbcd49e7b8dd9119ce33f76e.jpg?impolicy=1by1_wide_1242'),
-            width: 300,
-            height: 150,
-                
-            ),
+              Image(
+                image: NetworkImage(
+                    'https://globalassets.starbucks.com/assets/b635f407bbcd49e7b8dd9119ce33f76e.jpg?impolicy=1by1_wide_1242'),
+                width: 300,
+                height: 150,
+              ),
               Padding(
                 padding: const EdgeInsets.only(top: 16),
                 child: Text('Result: ${snapshot.data}'),
@@ -104,4 +99,3 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     );
   }
 }
-
