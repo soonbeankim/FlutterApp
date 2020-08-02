45c01c65e1adf9eb7a39abaa400c09df59a2bf99
Biny <binybing313@gmail.com>
2020-08-02 22:43:58 +0900

    listview.builder 로 바꿔보기 + time 보여주기

--- firstore_chatting/lib/main.dart
+++ firstore_chatting/lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final sendMessageController = TextEditingController();
+   final databaseReference = Firestore.instance;
   static const String _title = 'chatting';
 
   @override
@@ -18,8 +20,6 @@ class MyApp extends StatelessWidget {
             body: _buildBody(context)));
   }
 
-  final sendMessageController = TextEditingController();
-
   Widget _buildBody(BuildContext context) {
     return Column(
       children: <Widget>[
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
             builder: (context, snapshot) {
               if (!snapshot.hasData) return LinearProgressIndicator();
               return _buildList(context, snapshot.data.documents);
+              //snapshot.data = QuerySnapshot
             },
           ),
         ),
@@ -51,49 +52,66 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  final databaseReference = Firestore.instance;
-  void sendMessage() async {
-    print(sendMessageController);
+ 
+  void sendMessage(){
     databaseReference
         .collection("chatting")
-        .add({'name': '대훈', 'message': sendMessageController.text});
+        .add({'name': 'alex', 'message': sendMessageController.text, 'time': DateTime.now()});
   }
 }
 
 Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-  return ListView(
-    padding: const EdgeInsets.only(top: 20.0),
-    children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+  return ListView.builder(
+    itemCount: snapshot.length,
+    itemBuilder: (BuildContext ctxt, int index) {
+    //  return Text(snapshot[index].data.toString());
+    final record = Messages.fromSnapshot(snapshot[index]);
+
+    return Container(
+      child: ListTile(
+        title: Text(record.message) ,
+        subtitle: Text(record.time),
+        trailing: Text(record.name),
+      )
+    );
+    }
   );
 }
 
-Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-  final record = Messages.fromSnapshot(data);
+// Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+//   return ListView(
+//     padding: const EdgeInsets.only(top: 20.0),
+//     children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+//   );
+// }
 
-  return Container(
-    key: ValueKey(record.name),
-    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-    child: Column(
-      children: <Widget>[
-        ListTile(
-          title: Text(record.message),
-          trailing: Text(record.name),
-        ),
-      ],
-    ),
-  );
-}
+// Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+//   final record = Messages.fromSnapshot(data);
+
+//   return Container(
+//     key: ValueKey(data['name']),
+//     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//     child: 
+//         ListTile(
+//           title: Text(record.message),
+//           trailing: Text(record.name),
+//         ),
+//   );
+// }
 
 class Messages {
   final String name;
   final String message;
+  final String time;
   final DocumentReference reference;
 
   Messages.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['name'] != null),
         assert(map['message'] != null),
+        assert(map['time'] != null),
         name = map['name'],
-        message = map['message'];
+        message = map['message'],
+        time = map['time'].toDate().toString();
 
   Messages.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data, reference: snapshot.reference);
